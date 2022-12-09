f3f547839aa520dde9ccc51b72bfc3fdb63a5264
Christopher Brøndholt <c.broendholt@gmail.com>
2022-12-09 13:00:29 +0100

    Added part

--- day03/index.js
+++ day03/index.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 
-let hashmap = {}
+let hashmap = {};
 for (let i = 1; i <= 26; i++) {
-    hashmap[String.fromCharCode(i + 96)] = i
+  hashmap[String.fromCharCode(i + 96)] = i;
 }
 
 for (let i = 27; i <= 52; i++) {
   hashmap[String.fromCharCode(i + 38)] = i;
 }
 
-const input = fs.readFileSync('./input.txt', 'utf8');
-const split = input.split("\n")
+const input = fs.readFileSync("./input.txt", "utf8");
+const split = input.split("\n");
 
-function GetPriority(char){
-    return hashmap[char]
+function GetPriority(char) {
+  return hashmap[char];
 }
 
 function part1() {
@@ -30,33 +30,29 @@ function part1() {
         break;
       }
     }
-
   });
 
   console.log(result);
 }
 
-function part2(){
-    let result = 0
+function part2() {
+  let result = 0;
 
-    for(let j = 0; j < split.length; j += 3){
-        const group = split.slice(j, j + 3) 
+  for (let j = 0; j < split.length; j += 3) {
+    const group = split.slice(j, j + 3);
 
-        for (let i = 0; i < group[0].length; i++) {
-            if (group[1].includes(group[0][i]) && group[2].includes(group[0][i])) {
-                result += GetPriority(group[0][i]);
-                break;
-            }
-        }
+    for (let i = 0; i < group[0].length; i++) {
+      if (group[1].includes(group[0][i]) && group[2].includes(group[0][i])) {
+        result += GetPriority(group[0][i]);
+        break;
+      }
     }
+  }
 
-    console.log(result)
+  console.log(result);
 }
 
+const part = process.env.part || "part1";
 
-part1()
-part2()
-
-
-
-
+if (part === "part1") part1();
+else part2();
